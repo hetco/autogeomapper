b5d08ccc76c542d003f08e1337cf3717b29cdda9
SJohnson <simonbjohnson@gmail.com>
2021-08-16 17:42:05 +0100

    initial commit
    Based on community lens

---
+++ js/map.js
@@ -0,0 +1,87 @@
+/*function setLayerStyle(topic,featureAtt){
+
+	let mapColours = colours[topic];
+	let style = function (feature) {
+  		let colour = mapColours[feature.properties[featureAtt]-1];
+      	return { color: colour, weight: 0.5, fillOpacity: 0.5 }
+	}
+	featureLayer.setStyle(style);
+}
+
+function createMap(postcodes){
+	let map = L.map('map').setView([52,0], 6);
+	map.options.minZoom = 7;
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	featureLayer = L.esri.featureLayer({
+	  url: 'https://services3.arcgis.com/ivmBBrHfQfDnDf8Q/arcgis/rest/services/Indices_of_Multiple_Deprivation_(IMD)_2019/FeatureServer/0',
+	  simplifyFactor: 0.35,
+	  minZoom:10,
+	  precision: 5,
+	  renderer:L.canvas(),
+	  fields:['FID','IMDDec0','IncDec','EmpDec','EduDec','CriDec','BHSDec','EnvDec','HDDDec']
+	}).addTo(map);
+
+	let postcodeLayer = getPostcodeLayer(postcodes);
+	postcodeLayer.addTo(map);
+	
+
+    let info = L.control();
+
+    info.onAdd = function (map) {
+	    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+	    this.update();
+	    return this._div;
+	};
+
+	info.update = function (props) {
+    this._div.innerHTML = '<p id="zoomwarning">Please zoom in to see map layers</p>';
+	};
+
+	info.addTo(map);
+	info.update();
+
+	map.on('zoomend', function() {
+    	let zoom = map.getZoom();
+    	if(zoom<10){
+    		$('.info').show();
+    	} else {
+    		$('.info').hide();
+    	}
+
+	});
+
+	let bounds = postcodeLayer.getBounds();
+	map.fitBounds(bounds);
+}
+
+function getPostcodeLayer(postcodes,index){
+	let markers = [];
+	postcodes.forEach(function(postcode){
+		console.log(postcode);
+		let popUp = "<p>Postcode: "+postcode[0]+" - "+postcode[4]+"</p>";
+		popUp += '<p class="popuptext">IMD rank: '+postcode[5]+' out of 32,844</p>';
+		popUp += '<p class="popuptext">IMD Decile: '+getDecile(postcode[5])+'</p>';
+		popUp += '<p class="popuptext">Income Decile: '+getDecile(postcode[6])+'</p>';
+		popUp += '<p class="popuptext">Employment Decile: '+getDecile(postcode[7])+'</p>';
+		popUp += '<p class="popuptext">Education Decile: '+getDecile(postcode[8])+'</p>';
+		popUp += '<p class="popuptext">Health and Disability Decile: '+getDecile(postcode[9])+'</p>';
+		popUp += '<p class="popuptext">Crime Decile: '+getDecile(postcode[10])+'</p>';
+		popUp += '<p class="popuptext">Housing and Services Decile: '+getDecile(postcode[11])+'</p>';
+		popUp += '<p class="popuptext">Living Environment Decile: '+getDecile(postcode[12])+'</p>';
+		let marker = L.marker([postcode[1], postcode[2]]);
+		icon = marker.options.icon;
+		icon.options.iconSize = [20, 32];
+		icon.options.iconAnchor = [10,32];
+		icon.options.shadowSize = [0.0];
+		marker.setIcon(icon);
+		marker.bindPopup(popUp);
+		markers.push(marker);
+	});
+
+	let postcodeLayer = L.featureGroup(markers);
+	return postcodeLayer;
+}*/
---
+++ js/site.js
@@ -0,0 +1,206 @@
+function getPostCodeData(postcode){
+	let shortcode = postcode.substr(0,postcode.length-2);
+
+	let file = '../processed_data/'+shortcode+'.json';
+	console.log(file);
+	$.ajax(
+		{
+			url: file,
+			success: function(result){
+				if(result[postcode]==undefined){
+					errors.push(postcode);
+				} else {
+					let data = result[postcode];
+					data.unshift(postcode);
+		    		postcodeData.push(result[postcode]);
+		    							
+				}
+				asyncCalls--;
+	    		if(asyncCalls==0){
+	    			processingComplete();
+	    		}
+	    		updateProgress()
+	  		},
+	  		error: function(result){
+	  			errors.push(postcode);
+	  			asyncCalls--;
+	  			if(asyncCalls==0){
+	  				processingComplete();
+	    		}
+	    		updateProgress()
+	  		}
+  		}
+  	);
+}
+
+function updateProgress(){
+	let progress = total - asyncCalls;
+	$('#progress').html(progress+'/'+total);
+}
+
+function cleanPostcode(postcode){
+	postcode = postcode.replace(/ /g, '');
+	postcode = postcode.toUpperCase();
+	return postcode
+}
+
+function processingComplete(){
+	gtag('event', 'geocode_postcode', {'value':postcodeData.length});
+	if(errors.length>0){
+		$('#analysis-progress').hide();
+		$('#analysis-finished').show();
+		$('#error-count').html(errors.length);
+		$('#errors').html('');
+		errors.forEach(function(error){
+			$('#errors').append('<p class="errorpostcode">'+error+'</p>')
+		});
+	} else {
+		$('#step1').hide();
+		$('#step2').show();
+	}
+
+}
+
+function loadAnalysisStep(topic){
+	$('#step2').hide();
+	$('#step3').show();
+	createMap(postcodeData);
+	setForData(topic);
+}
+
+function setForData(topic){
+	console.log('setting for topic: '+topic);
+
+	console.log(dataIndex[topic].definition);
+
+	$('#title').html(dataIndex[topic].title);
+	$('#definition').html(dataIndex[topic].definition);
+	$('#datasource').attr("href",dataIndex[topic].source);
+	$('#decile1def').html(dataIndex[topic].decile1Def);
+	$('#decile10def').html(dataIndex[topic].decile10Def);
+
+	let index = dataIndex[topic].index;
+	let featureAtt = dataIndex[topic].featureAtt;
+
+	setLayerStyle(topic,featureAtt);
+	averageRank = getAverageRank(postcodeData,index,topic);
+	rankBar('#averageRank',averageRank,[],topic);
+	barChart('#barchart',postcodeData,index,topic);
+	generateText(postcodeData,index,topic);
+	$('.btn-data').removeClass('btn-active');
+	$('.btn[data-id="'+topic+'"]').addClass('btn-active');
+
+
+}
+
+function getDecile(rank){
+	let decile = Math.floor(parseInt(rank)/32844*10)+1
+	if(decile>10){
+		decile = 10;
+	}
+	return decile
+}
+
+function generateDownload(){
+	let downloadData = [["Postcode", "Latitude", "Longitude", "LA Code", "Local Authority", "IMD Decile", "Income Decile", "Employment Decile", "Education Decile", "Health and Disability Decile", "Crime Decile", "Housing and Services Decile", "Living Environment Decile"]];
+	postcodeData.forEach(function(row){
+		downloadData.push([row[0],row[1],row[2],row[3],row[4],getDecile(row[5]),getDecile(row[6]),getDecile(row[7]),getDecile(row[8]),getDecile(row[9]),getDecile(row[10]),getDecile(row[11]),getDecile(row[12])]);
+	});
+
+	let csvContent = "data:text/csv;charset=utf-8,";
+
+	downloadData.forEach(function(rowArray) {
+	    let row = rowArray.join(",");
+	    csvContent += row + "\r\n";
+	});
+
+	var encodedUri = encodeURI(csvContent);
+	var link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", "community_lens.csv");
+	document.body.appendChild(link);
+	link.click();
+
+}
+
+function init(){
+	$('.process-postcodes').on('click',function(){
+		$('#process-postcodes').hide();
+		$('#analysis-finished').hide();
+		$('#analysis-progress').show();
+		postcodeData = [];
+		errors = [];
+		let postcodesList = $('#postcode_entry_text').val();
+		let postcodes = postcodesList.split(/\n/);
+		total = postcodes.length;
+		asyncCalls = postcodes.length;
+		postcodes.forEach(function(postcode){
+			postcode = cleanPostcode(postcode);
+			getPostCodeData(postcode);
+		});
+	});
+
+	$('#continue').on('click',function(){
+		$('#step1').hide();
+		$('#step2').show();
+	});
+
+	$('.btn-data').on('click',function(){
+		setForData($(this).attr('data-id'));
+	})
+
+	$('#download-data').on('click',function(){
+		generateDownload();
+	});
+
+	$('#step2').hide();
+	$('#step3').hide();
+
+	$('#analysis-progress').hide();
+	$('#analysis-finished').hide();
+
+	$('.btn-datatopic').on('mouseover',function(){
+		let topic = $(this).attr('data-id');
+		let title = dataIndex[topic].title;
+		let definition = dataIndex[topic].definition;
+		let source = dataIndex[topic].source;
+		$('#dataset-title').html(title);
+		$('#dataset-definition').html(definition);
+		console.log(source);
+		$('#dataset-source').attr('href',source);
+	});
+
+	$('.btn-datatopic').on('click',function(){
+		let topic = $(this).attr('data-id');
+		loadAnalysisStep(topic);
+	});
+}
+
+var postcodeData = [];
+var errors = []
+var asyncCalls;
+var featureLayer;
+var total;
+let colours = {
+	'IMD':['#E65100','#EF6C00','#F57C00','#FB8C00','#FF9800','#FFA726','#FFB74D','#FFCC80','#FFE0B2','#FFF3E0'],
+	'Extra':['#40004b','#762a83','#9970ab','#c2a5cf','#e7d4e8','#d9f0d3','#a6dba0','#5aae61','#1b7837','#00441b'],
+	'Income':['#01579B','#0277BD','#0288D1','#039BE5','#03A9F4','#29B6F6','#4FC3F7','#81D4FA','#B3E5FC','#E1F5FE'],
+	'Employment':['#311B92','#4527A0','#512DA8','#5E35B1','#673AB7','#7E57C2','#9575CD','#B39DDB','#D1C4E9','#EDE7F6'],
+	'Education':['#004D40','#00695C','#00796B','#00897B','#009688','#26A69A','#4DB6AC','#80CBC4','#B2DFDB','#E0F2F1'],
+	'Health':['#880E4F','#AD1457','#C2185B','#D81B60','#E91E63','#EC407A','#F06292','#F48FB1','#F8BBD0','#FCE4EC'],
+	'Crime':['#F57F17','#F9A825','#FBC02D','#FDD835','#FFEB3B','#FFEE58','#FFF176','#FFF59D','#FFF9C4','#FFFDE7'],
+	'Housing':['#3E2723','#4E342E','#5D4037','#6D4C41','#795548','#8D6E63','#A1887F','#BCAAA4','#D7CCC8','#EFEBE9'],
+	'Environment':['#1B5E20','#2E7D32','#388E3C','#43A047','#4CAF50','#66BB6A','#81C784','#A5D6A7','#C8E6C9','#E8F5E9']
+	};
+init();
+
+
+
+
+
+
+
+
+
+
+
